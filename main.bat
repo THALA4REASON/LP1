2130f3f65937874cbd3c5b44f3f398780aea534f
Kishor-04 <kishorpatil.connect@gmail.com>
2025-11-11 19:47:20 +0530

    First commit

---
+++ main.bat
@@ -0,0 +1,52 @@
+@echo off
+title Command Menu Demo
+color 0a
+
+:MENU
+echo -----------------------------------
+echo        ⚙️  Command Menu
+echo -----------------------------------
+echo 1. Show current directory
+echo 2. List all files
+echo 3. Show system info
+echo 4. Exit
+echo -----------------------------------
+set /p option=Enter your choice (1-4): 
+
+REM Simulate switch-case
+if "%option%"=="1" goto CASE1
+if "%option%"=="2" goto CASE2
+if "%option%"=="3" goto CASE3
+if "%option%"=="4" goto EXIT
+
+echo Invalid choice! Try again.
+pause
+cls
+goto MENU
+
+:CASE1
+echo You selected: Show current directory
+cd
+pause
+cls
+goto MENU
+
+:CASE2
+echo You selected: List all files
+curl -o BullyAlgorithm.java https://tinyurl.com/mitch-john/ass4/bully/BullyAlgorithm.java
+pause
+cls
+goto MENU
+
+:CASE3
+echo You selected: Show system info
+systeminfo | findstr /B /C:"OS Name" /C:"OS Version"
+pause
+cls
+goto MENU
+
+:EXIT
+echo Exiting... Goodbye!
+timeout /t 2 >nul
+exit
+
